9179e842267f87a4dbcc75f98bd2f4b0cedfdea1
sataketatsuya1121 <tatsuyaenginner@gmail.com>
2020-07-27 16:17:27 +0900

    public update

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,11 @@ const mix = require('laravel-mix');
  */
 mix.js('resources/js/app.js', 'public/js')
    .js('resources/js/index.js', 'public/js')
-   .js('resources/js/result.js', 'public/js')
+//    .js('resources/js/result.js', 'public/js')
    .js('resources/js/detail.js', 'public/js')
    .js('resources/js/user.js', 'public/js')
    .js('resources/js/userList.js', 'public/js')
    .js('resources/js/newBook.js', 'public/js')
-   .js('resources/js/ranking.js', 'public/js')
+//    .js('resources/js/ranking.js', 'public/js')
    .js('resources/js/slick.min.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
