68d80ae1b2341c4f067b80accf441976c68f0d83
sataketatsuya1121 <tatsuyaenginner@gmail.com>
2020-07-27 14:31:51 +0900

    channel update

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('rankUpChannel', function () {
+    return true;
 });
+
+Broadcast::channel('registerChannel', function () {
+    return true;
+});
+
+Broadcast::channel('reviewChannel', function () {
+    return true;
+});
+
