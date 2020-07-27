083fa30f81d6c72b4e0c16adba621f7910c70071
sataketatsuya1121 <tatsuyaenginner@gmail.com>
2020-07-27 14:33:24 +0900

    api update

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['api']], function () {
+    Route::post('/reviewCount', 'Api\BookReviewController@createReviewCountApi')->name('reviewCountApi');
+    Route::post('/starCount', 'Api\BookReviewController@createStarCountApi')->name('starCountApi');
 });
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::post('/notification', 'Api\BookReviewController@createNotificationApi')->name('notificationApi');
+});
+
