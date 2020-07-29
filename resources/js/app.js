c427371f3ea3fbf8fe8bfd86a1ba56d99805b324
sataketatsuya1121 <tatsuyaenginner@gmail.com>
2020-07-29 14:41:54 +0900

    api auth update

--- resources/js/app.js
+++ resources/js/app.js
@@ -294,7 +294,7 @@ $(function(){
 
   for(var c of cookiesArray){ //一つ一つ取り出して
     var cArray = c.split('='); //さらに=で分割して配列に
-    if( cArray[0] == ' api_token'){ // 取り出したいkeyと合致したら
+    if(cArray[0] == ' api_token' || cArray[0] == 'api_token'){ // 取り出したいkeyと合致したら
         apiToken = cArray[1];  // [key,value]
     }
   }
